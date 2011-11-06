444d87a2f0456d4aa5603d1100c621807bdedb90
Chimo <stephberube@gmail.com>
2011-11-06 14:32:06 -0500

    Separated logic from presentation. Added Dreamweaver extension.

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-# Browser-based XHTML 1.0 Strict to HTML5 batch conversion
+# XHTML 1.0 Strict to HTML5 batch conversion
 
 I came across a situation where I needed to convert a bunch of XHTML 1.0 Strict documents (or snippets) to valid HTML5.  
 This tool automates the process.
 
 Note that this will not _add_ any new HTML5 elements/features to your pages.  
-It merely gets rid of deprecated XHTML 1.0 Strict elements/attributes.
+It merely gets rid of deprecated XHTML 1.0 Strict elements/attributes or replaces them with conforming ones.
 
-## Steps
+## Usage
 
-1. Download and extract the files. (or use the [demo here](http://chimo.github.com/x2h/))
-1. Visit index.html in Firefox 3.6+ (if you are using Chrome/Chromium, see note below)
-1. Drag/drop your valid XHTML 1.0 Strict files in the marked area.
-1. If everything goes according to plan you'll be asked to download a zip file containing the HTML5 equivalent of your files.
-1. For more information (or if the conversion failed), take a look at the "Output" area. Messages will be added there as the files are processed.
+There are two "products" in here:
+
+1. Browser-based, drag-and-drop batch conversion:
+    1. Download and extract the files. (or use the [demo here](http://chimo.github.com/x2h/))
+    1. Visit index.html in Firefox 3.6+ (if you are using Chrome/Chromium, see note below)
+    1. Drag/drop your valid XHTML 1.0 Strict files in the marked area.
+    1. If everything goes according to plan you'll be asked to download a zip file containing the HTML5 equivalent of your files.
+    1. For more information (or if the conversion failed), take a look at the "Output" area. Messages will be added there as the files are processed.
+1. Dreamweaver extension
+    1. Open "dw-x2h.mxi" with the "Adobe Extension Manager"
+    1. Access 'XHTML to HTML5' entry in the 'Commands' menu
+    1. Select the folder containing your XHTML files
+    1. Wait while DW converts the files
+    1. At the end, you'll be left with an opened document containing notes and information on the conversion, if applicable.
 
 ## What exacly does this do to my documents?
 
@@ -37,5 +46,6 @@ It merely gets rid of deprecated XHTML 1.0 Strict elements/attributes.
 * If you want to use this locally (file://) with Chrome/Chromium you need to start the browser with: _--allow-file-access-from-files --allow-file-access_
   Otherwise you'll get "Security error".
 * The tool has been tested with: 
-    * Firefox 3.6.21, 6.0.2, 8.0a2
+    * Firefox 3.6.21, 6.0.2, 7.0.1, 8.0a2
     * Chromium 14.0.835.186
+    * Dreamweaver CS5
--- js/x2h.js
+++ js/x2h.js
@@ -1,182 +1,8 @@
 // Tested on:
-//  * Firefox 3.6.21, 6.0.2, 8.0a2
+//  * Firefox 3.6.21, 6.0.2, 7.0.1, 8.0a2
 //  * Chromium 14.0.835.186
 
-/**
- * Returns whether the zip file is empty or not
- *
- * @returns  true if the zip file is empty false otherwise
- */
-JSZip.prototype.isEmpty = function() {
-   for (var filename in this.files) {
-       return false;
-   }
-
-   return true;
-}
-
 var x2h = {
-    // Global counter
-    completedCount: 0,
-
-    // Init
-    init: function() {
-        // Messages
-        x2h.output = document.getElementById('output');
-        
-        // Setup the dnd listeners.
-        x2h.dropZone = document.getElementById('drop_zone'),
-        x2h.dropZone.addEventListener('dragover', x2h.handleDragOver, false);
-        x2h.dropZone.addEventListener('drop', x2h.handleFileSelect, false);
-    },
-
-    // Package the results in a zip file and offer download (or show notice if nothing to download)
-    finalize: function() {
-        if(!x2h.zip.isEmpty()) {
-            x2h.output.innerHTML += '<li>Done!</li>';
-            content = x2h.zip.generate();
-            location.href='data:application/zip;base64,'+content
-        }
-        else {
-            x2h.output.innerHTML += '<li>All given files were skipped. Nothing to download.</li>';
-        }
-        
-        // Re-apply event listener so that we can drop more files
-        x2h.dropZone.addEventListener('dragover', x2h.handleDragOver, false);
-        x2h.dropZone.addEventListener('drop', x2h.handleFileSelect, false);
-        x2h.dropZone.innerHTML = 'Drop XHTML files here';
-    },
-
-    // Modified from here: http://www.html5rocks.com/en/tutorials/file/dndfiles/#toc-selecting-files-dnd
-    handleFileSelect: function(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-
-        // FileList object
-        var files = evt.dataTransfer.files;
-        // This sometimes happen for whatever reason (with Thunar + Firefox, for example)
-        if(files.length == 0) {
-            x2h.output.innerHTML = 'Number of files dropped was zero';
-            return;
-        }
-
-        // Clear old messages
-        x2h.output.innerHTML = '';
-        
-        // Initialize the resulting zip file
-        x2h.zip = new JSZip();
-
-        // Remove event listener so that we can't drop more files until we're done.    
-        x2h.dropZone.removeEventListener('dragover', x2h.handleDragOver, false);
-        x2h.dropZone.removeEventListener('drop', x2h.handleFileSelect, false);    
-        
-        x2h.dropZone.innerHTML = 'Please wait...';
-        x2h.completedCount = 0; // Ensure we're starting from zero when we're given a new batch of files to process
-
-        // files is a FileList of File objects
-        for (var i = 0, f; f = files[i]; i++) {
-            // Skip non-HTML files
-            if (!f.type.match('text/html')) {
-                x2h.output.innerHTML += '<li>File <em>' + f.name + '</em> doesn\'t seem to be an HTML file. Skipping.</li>';
-
-                x2h.completedCount++;
-                if(x2h.completedCount == files.length) {
-                    x2h.finalize();
-                }
-
-                continue;
-            }
-            var reader = new FileReader();
-
-            // FIXME: I could not get Firefox to use the onerror event handler. Even when getting: 
-            // Component returned failure code: 0x80520012 (NS_ERROR_FILE_NOT_FOUND) [nsIDOMFileReader.readAsText]
-            reader.onerror = (function(theFile) {
-                return function(e) {
-                    var msg = '';
-                    switch(e.target.error.code) {
-                        case FileError.NOT_FOUND_ERR:
-                            msg = 'File not found';
-                            break;
-                        case FileError.SECURITY_ERR:
-                            msg = 'Security error';
-                            break;
-                        case FileError.ABORT_ERR:
-                            msg = 'Operation aborted';
-                            break;
-                        case FileError.NOT_READABLE_ERR:
-                            msg = 'File not readable';
-                            break;
-                        case FileError.ENCODING_ERR:
-                            msg = 'Encoding error';
-                            break;
-                        default:
-                            msg = 'Unknown error code: ' + e.target.error.code;
-                    }
-
-                    x2h.output.innerHTML += '<li>Error reading file: <em>' + theFile.name + '</em> (' + msg + '). Skipping.</li>';
-                };
-            })(f);
-
-            // Callback function after the file is read
-            reader.onload = (function(theFile) {
-                return function(e) { 
-                    // XHTML 1.0 Strict to HTML5 cleanup
-                    var html5Content = ''; 
-                    
-                    try {
-                        html5Content = x2h.xhtmlToHtml5(e.target.result, theFile.name);
-    
-                        // Add the clean file to the zip archive
-                        x2h.zip.add(theFile.name, html5Content);
-                        x2h.dropZone.innerHTML = 'File ' + (x2h.completedCount+1) + ' of ' + files.length + ' completed';
-                    }
-                    catch(err) {
-                        var srcTxt = err.getElementsByTagName('sourcetext')[0];
-
-                        var perr = err.getElementsByTagName('parsererror')[0];
-                        var srcTxt = perr.removeChild(srcTxt);
-
-                        var li = x2h.output.appendChild(document.createElement('li'));
-
-                        var arr = perr.textContent.split('\n');
-                        for(var i=0; i<arr.length; i++) {
-                            li.appendChild(document.createTextNode(arr[i]));
-                            li.appendChild(document.createElement('br'));
-                        }
-
-                        arr = srcTxt.textContent.split('\n');
-                        for(var i=0; i<arr.length; i++) {
-                            li.appendChild(document.createTextNode(arr[i]));
-                            li.appendChild(document.createElement('br'));
-                        }
-                    }
-
-                    // Keep track of how many files have been processed
-                    x2h.completedCount++; 
-                    
-                    // If this is the last file, generate the .zip and offer the download
-                    if(x2h.completedCount == files.length) {
-                        x2h.finalize();
-                    }
-                };
-            })(f);
-
-            // Read the file
-            reader.readAsText(f);        
-        }
-    },
-
-    // From here: http://www.html5rocks.com/en/tutorials/file/dndfiles/#toc-selecting-files-dnd
-    /**
-     * Prevents propagation and default behavior an event (in our case the "dragover" event)
-     *
-     * @param evt   Event fired (in our case the "dragover" event)
-     */
-    handleDragOver: function(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-    },
-
     // TODO: Accept a 'map' of elements/attributes
     /**
      * Removes one or more attribtues on one or more elements
@@ -213,6 +39,8 @@ var x2h = {
             }
         }
     },
+    
+    msgs: [],
 
     /**
      * Performs multiple operations to convert a XHTML 1.0 Strict to HTML5
@@ -221,6 +49,7 @@ var x2h = {
      * @returns     String containing valid HTML5
      */
     xhtmlToHtml5: function(html, filename) {
+        x2h.msgs = [];
 
         // DOMParser() chokes on Doctypes, so remove it and add HTML5 Doctype at the end
         var dtd_re = /<!doctype.*\r?\n?.*\.dtd">/i; // FIXME: That's one sucky regex right there...
@@ -238,7 +67,7 @@ var x2h = {
         html =  html.replace(/<acronym/g, '<abbr');
         html =  html.replace(/<\/acronym/g, '</abbr');
  
-        // Replace <big> with existing @class with <span> with existing @class plus "x2h-big"
+        // Replace <big> with existing @class with <span> with existing @class plus "big"
         // Regex is a modification of one found in htmlparser.js (http://ejohn.org/blog/pure-javascript-html-parser/)
         html = html.replace(/<big((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)((?:\s+class(?:\s*=\s*(?:(?:"([^"]*)")|(?:'([^']*)')|[^>\s]+))?))((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/g, '<span$1 class="$3$4 big"$5>');
 
@@ -246,7 +75,7 @@ var x2h = {
         html =  html.replace(/<big/g, '<span class="big"');
         html =  html.replace(/<\/big/g, '</span');
 
-        // Replace <tt> with existing @class with <span> with existing @class plus "x2h-tt"
+        // Replace <tt> with existing @class with <span> with existing @class plus "tt"
         // Regex is a modification of one found in htmlparser.js (http://ejohn.org/blog/pure-javascript-html-parser/)
         html = html.replace(/<tt((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)((?:\s+class(?:\s*=\s*(?:(?:"([^"]*)")|(?:'([^']*)')|[^>\s]+))?))((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/g, '<span$1 class="$3$4 tt"$5>');
         
@@ -254,6 +83,7 @@ var x2h = {
         html =  html.replace(/<tt/g, '<span class="tt"');
         html =  html.replace(/<\/tt/g, '</span');    
 
+        // Build the DOM
         var parser = new DOMParser();
         html = parser.parseFromString(html, "text/xml");
 
@@ -265,7 +95,7 @@ var x2h = {
         var cells = '';
         var attr = '';
         var clss = '';
-
+        
         // Processing <table>s
         for(var i=0; i<tbls.length; i++) {
             tbls[i].removeAttribute('summary'); // Remove @summary
@@ -277,7 +107,7 @@ var x2h = {
                     clss = clss + ' ';
                 }
                 tbls[i].setAttribute('class', clss + 'cellspacing' + attr);
-                x2h.output.innerHTML += '<li><em>' + filename + '</em> is using class: <em>cellspacing' + attr + '</em></li>';
+                x2h.msgs.push(filename + ' is using class: cellspacing' + attr);
                 tbls[i].removeAttribute('cellspacing'); // Remove @cellspacing
             }
 
@@ -289,40 +119,34 @@ var x2h = {
                     clss = clss + ' ';
                 }
                 tbls[i].setAttribute('class', clss + 'cellpadding' + attr);
-                x2h.output.innerHTML += '<li><em>' + filename + '</em> is using class: <em>cellpadding' + attr + '</em></li>';
+                x2h.msgs.push(filename + ' is using class: cellpadding' + attr);
                 tbls[i].removeAttribute('cellpadding'); // Remove @cellpadding
             }
         }
 
-//        x2h.rmAttrs(html.getElementsByTagName('table'), ['summary','cellpadding','cellspacing']); // Replaced with the above block
-        x2h.rmAttrs(html.getElementsByTagName('area'), 'nohref');
-        x2h.rmAttrs(html.getElementsByTagName('head'), 'profile');
-        x2h.rmAttrs(html.getElementsByTagName('object'), ['archive','classid','codebase','codetype','declare','standby']);
-        x2h.rmAttrs(html.getElementsByTagName('param'), ['valuetype', 'type']);
-        x2h.rmAttrs(html.getElementsByTagName('a'), ['name','rev','charset','shape','coords']); // TODO: Check if the doc has an id.value of this.name.value somewhere. If not, add an @id of this.name.value to this element
-        x2h.rmAttrs(html.getElementsByTagName('link'), ['rev', 'charset']);
-        x2h.rmAttrs(html.getElementsByTagName('td'), ['axis', 'abbr', 'scope', 'valign']);
-        x2h.rmAttrs(html.getElementsByTagName('th'), ['axis', 'abbr', 'valign']);
-        x2h.rmAttrs(html.getElementsByTagName('meta'), 'scheme');
-        x2h.rmAttrs(html.getElementsByTagName('img'), 'longdesc');
+//        this.rmAttrs(html.getElementsByTagName('table'), ['summary','cellpadding','cellspacing']); // Replaced with the above block
+        this.rmAttrs(html.getElementsByTagName('area'), 'nohref');
+        this.rmAttrs(html.getElementsByTagName('head'), 'profile');
+        this.rmAttrs(html.getElementsByTagName('object'), ['archive','classid','codebase','codetype','declare','standby']);
+        this.rmAttrs(html.getElementsByTagName('param'), ['valuetype', 'type']);
+        this.rmAttrs(html.getElementsByTagName('a'), ['name','rev','charset','shape','coords']); // TODO: Check if the doc has an id.value of this.name.value somewhere. If not, add an @id of this.name.value to this element
+        this.rmAttrs(html.getElementsByTagName('link'), ['rev', 'charset']);
+        this.rmAttrs(html.getElementsByTagName('td'), ['axis', 'abbr', 'scope', 'valign']);
+        this.rmAttrs(html.getElementsByTagName('th'), ['axis', 'abbr', 'valign']);
+        this.rmAttrs(html.getElementsByTagName('meta'), 'scheme');
+        this.rmAttrs(html.getElementsByTagName('img'), 'longdesc');
 
         var results = '';
 
+        // DOM to String
         var s = new XMLSerializer();
         results = s.serializeToString(html);
-
+            
         // If we removed the original dtd, put the HTML5 in its place
         if(dtd != null) {
             results  = '<!DOCTYPE html>' + results;
         }
 
         return results;
-    },
-}
-
-// Init Call at Runtime
-document.onreadystatechange = function () {
-    if (document.readyState == 'complete') {
-        x2h.init();
     }
 }
